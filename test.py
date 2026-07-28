4bc270889d8ff055c0e06a84e4879bc910ff6986
seungmin <kangga89@naver.com>
2026-07-28 17:43:38 +0900

    폭풍

--- test.py
+++ test.py
@@ -1,41 +1,10 @@
-print("Hello World!")
-print("안녕하세요!")
+temps = [30, 28, 27, 26, 24, 24, 22]
 
-print("===1번 설비 측정값===")
-print("온도():", 75)
-print("진동():", 2.3)
+temps.sort()
+print(temps)
 
-print("두 측정값의 합:", 75 + 80)
-print("온도 변화량:", 80 - 75)
-print("평균 온도:", (75 + 80) / 2)
+temps.reverse()
+print(temps)
 
-print("두 측정값의 합:", 43 + 21)
-print("온도 변화량:", 43 - 21)
-print("평균 온도:", (43 + 21) / 2)
-
-print("정상 온도:", 34)
-print("현재 온도:", 44)
-print("차이 계산 직전:", 44 - 34)  # 중간값 확인
-print("온도 상승량:", 44 - 34)
-
-# 1번 설비 측정값 출력
-print("온도:", 75)
-print("진동:", 2.3)
-
-print("온도:", 75)
-print("진동:", 2.3)
-print("압력:", 4.0)
-
-temp = 25
-Temp = 100
-print(temp)
-print(Temp)
-
-a = 10
-b = a
-a = 90
-print(b)
-
-x, y, z = 10, 2, 5
-print(x)
-print(z)
+print(temps.count(28))
+print(temps.index(22))
