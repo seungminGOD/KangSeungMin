d29f597bbf0a58df4a14509d52a6fe1d413d4256
seungmin <kangga89@naver.com>
2026-07-29 15:51:28 +0900

    수정22

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-temps = [30, 28, 27, 26, 24, 24, 22]
-
-temps.sort()
-print(temps)
-
-temps.reverse()
-print(temps)
-
-print(temps.count(28))
-print(temps.index(22))
+total = 0  # 누적 변수 0으로 시작
+for i in range(1, 6):  # i = 1,2,3,4,5
+    total += i  # total = total + i
+print("최종 합계:", total)  # 15
